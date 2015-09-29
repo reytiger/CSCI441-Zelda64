9123cd176f420ee165c1d354fd0e026d32e2135c
Nico Pampe <nicopampe@gmail.com>
2015-09-29 09:50:33 -0600

    Tried working a bit with clang-formate

--- include/cameras/ArcBallCamera.hpp
+++ include/cameras/ArcBallCamera.hpp
@@ -6,11 +6,11 @@
  *
 */
 
-#ifdef __APPLE__ // if compiling on Mac OS
+#ifdef __APPLE__        // if compiling on Mac OS
 #include <GLUT/glut.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
-#else // else compiling on Linux OS
+#else                   // else compiling on Linux OS
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
