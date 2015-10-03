66afb43f564607cbce7469090b0c7e0ef31e7875
Nico Pampe <nicopampe@gmail.com>
2015-10-03 10:57:39 -0600

    Updated initScene

--- source/main.cpp
+++ source/main.cpp
@@ -6,7 +6,33 @@
 // called.
 void updateScene(double, double) {}
 
-void initScene() {}
+void initScene() {
+  glEnable(GL_DEPTH_TEST);
+  //******************************************************************
+  // this is some code to enable a default light for the scene;
+  // feel free to play around with this, but we won't talk about
+  // lighting in OpenGL for another couple of weeks yet.
+  float lightCol[4] = {1, 1, 1, 1};
+  float ambientCol[4] = {0.0, 0.0, 0.0, 1.0};
+  float lPosition[4] = {10, 10, 10, 1};
+  glLightfv(GL_LIGHT0, GL_POSITION, lPosition);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, lightCol);
+  glLightfv(GL_LIGHT0, GL_AMBIENT, ambientCol);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+
+  // tell OpenGL not to use the material system; just use whatever we
+  // pass with glColor*()
+  glEnable(GL_COLOR_MATERIAL);
+  glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+  //******************************************************************
+
+  glShadeModel(GL_FLAT);
+
+  srand(time(NULL)); // seed our random number generator
+                     // TODO: set up the world
+                     // generateEnvironmentDL();
+}
 
 int main(int argc, char **argv) {
   srand(time(nullptr));
