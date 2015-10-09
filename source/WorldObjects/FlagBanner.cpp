938272826c6e53ac5f099d8177d58bd82e3c8812
Nico Pampe <nicopampe@gmail.com>
2015-10-08 18:12:32 -0600

    OMG! It's a flag! I can't even.
     -Tall, red flag is drawn.
     -Moves
     -Should be dynamic, different controle points should effect all elements of the flag.

--- source/WorldObjects/FlagBanner.cpp
+++ source/WorldObjects/FlagBanner.cpp
@@ -12,8 +12,9 @@ void FlagBanner::draw() const {
         glTranslatef(0, 2, 5);
         drawControlPoints();
         drawPole();
-        drawFlag();
         m_WindsCurve.draw();
+        glTranslatef(0, 4, 0);
+        drawFlag();
     });
 }
 
@@ -25,27 +26,69 @@ void FlagBanner::drawControlPoints() const {
 
 void FlagBanner::drawFlag() const {
     pushMatrixAnd([&]() {
-
+        // TODO: culling? lighting? What should we do here
+        // TODO: set a texture.
+        // num of points to draw
+        double stepSize = 0.1;
+        double height = 5;
+        glDisable(GL_CULL_FACE);
+        // glDisable(GL_LIGHTING);
+        glColor3f(210 / 255.0,
+                  40 / 255.0,
+                  45 / 255.0); // nice dark brown for the base
+        glBegin(GL_TRIANGLES);
+        {
+            for (double i = 0; i < 1 - stepSize; i += stepSize) {
+                Vec drawing = m_WindsCurve.eval(i);
+                Vec next    = m_WindsCurve.eval(i + stepSize);
+                // first triangle
+                glVertex3f(drawing.x, drawing.y, drawing.z);
+                glVertex3f(drawing.x, drawing.y + height, drawing.z);
+                glVertex3f(next.x, next.y, next.z);
+                // second trianlge
+                glVertex3f(drawing.x, drawing.y + height, drawing.z);
+                glVertex3f(next.x, next.y, next.z);
+                glVertex3f(next.x, next.y + height, next.z);
+            }
+        };
+        glEnd();
+        glEnable(GL_CULL_FACE);
+        // glEnable(GL_LIGHTING);
     });
 }
 
 void FlagBanner::drawPole() const {
     pushMatrixAnd([&]() {
-
+        // base of pole
+        Vec target = m_WindsCurve.getVec().at(0);
+        glTranslatef(target.x, 0, target.z);
+        // turn so its faccing up
+        glRotatef(-90, 1, 0, 0);
+        glColor3f(153 / 255.0,
+                  89 / 255.0,
+                  45 / 255.0); // nice dark brown for the base
+        GLUquadricObj *quadratic;
+        quadratic = gluNewQuadric();
+        gluCylinder(quadratic, 1, 0.8, 10, 20, 2);
     });
 }
 
 void FlagBanner::updateAnimation(double t, double dt) {
     // TODO: create a more relistic movement
     std::vector<Vec> currentVec = m_WindsCurve.getVec();
+    double speed                = 4;
     // point 1
     Vec newCPoint = currentVec.at(1);
-    newCPoint.z = sin(currentVec.at(1).z + t * 2);
+    newCPoint.z = sin(currentVec.at(1).z + t * speed);
     currentVec.at(1) = newCPoint;
     // point 2
     newCPoint   = currentVec.at(2);
-    newCPoint.z = cos(currentVec.at(2).z + t * 2);
+    newCPoint.z = -sin(currentVec.at(2).z + t * speed);
     currentVec.at(2) = newCPoint;
+    // point 3
+    newCPoint   = currentVec.at(3);
+    newCPoint.z = -sin(currentVec.at(3).z + t * speed);
+    currentVec.at(3) = newCPoint;
     m_WindsCurve.setVec(currentVec);
     m_WindsCurve.recomputeCurve(100);
 }
