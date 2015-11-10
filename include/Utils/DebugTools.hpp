cd87802bbe739364ab48afab50ae7bacf14d8c09
Chris Butler <chrbutle@mines.edu>
2015-11-09 17:11:30 -0700

    Wrapped glChk() in an #ifdef - it won't run in a release build.

--- include/Utils/DebugTools.hpp
+++ include/Utils/DebugTools.hpp
@@ -1,14 +1,27 @@
 #pragma once
 
+void trace_helper(const char *file, int line, const char *func);
+void check_helper(const char *file, int line);
+
+#ifdef NDEBUG
+
+#define trace()
+#define check_error()
+#define glChk()
+
+#else
+
 // This prints traces as mangled function names. This command makes life easier:
 // ./a2 2>&1 | grep a1 | tr '()+' ' ' | awk '{ printf "%s\n", $2; }'
 // Then pipe that to c++-filt (which isn't on Alamode!) or use
 // http://demangler.com.
 #define trace() trace_helper(__FILE__, __LINE__, __FUNCTION__)
-void trace_helper(const char *file, int line, const char *func);
 
 // Error checking function which we can turn off later, if we need to, to catch
 // generic errors: errno, OpenGL, and (maybe?) FMOD.
 #define check_error() check_helper(__FILE__, __LINE__)
-void check_helper(const char *file, int line);
+
+// I don't want to rename it yet.
 #define glChk check_error
+
+#endif
