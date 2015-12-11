b6bf16cc3f37da52bd5e7034de3b3e884577a2fd
Chris Butler <chrbutle@mines.edu>
2015-12-10 22:21:19 -0700

    Fixed a typo in the Luminosity shader.

--- glsl/luminosity/frag.glsl
+++ glsl/luminosity/frag.glsl
@@ -5,6 +5,6 @@ uniform sampler2D fbo;
 
 void main() {
     vec4 texel = texture2D(fbo, st);
-    texel.rgb = 0.21 * texel.r + 0.72 * texel.g + 0.07 * texel.g;
+    texel.rgb = 0.21 * texel.r + 0.72 * texel.g + 0.07 * texel.b;
     gl_FragColor = texel;
 }
